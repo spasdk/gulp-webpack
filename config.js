b708d3131e79099f890eb5683838e9751c852eec
Stanislav Kalashnik <sk@infomir.eu>
2016-01-16 15:12:12 +0200

    rework base config to extent root config

--- config.js
+++ config.js
@@ -1,74 +1,49 @@
 /**
  * Webpack configuration for webpack gulp task.
  *
- * @see http://webpack.github.io/docs/configuration.html
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
  */
 
 'use strict';
 
-// public
-module.exports = {
-    // turn on/off compilation
-    active: true,
+var path   = require('path'),
+    extend = require('extend'),
+    config = require('spa-gulp/config');
 
-    // directory to store output files
-    // default: <project root>/app/js/
-    path: 'js',
 
-    // config for webpack:build:develop task
-    develop: {
-        // intended output file name
-        targetFile: 'develop.js',
+// base config
+// each profile inherits all options from the "default" profile
+module.exports = extend(true, {}, config, {
+    default: {
+        // directory to look for source files
+        sourcePath: path.join(config.default.sourcePath, 'js'),
 
-        //// whether to use space or tab character for indentation
-        //indentType: 'space',
-		//
-        //// the number of spaces or tabs to be used for indentation
-        //indentWidth: 4,
-		//
-        //// whether to use cr, crlf, lf or lfcr sequence for line break
-        //linefeed: 'lf',
-		//
-        //// output format of the final CSS style
-        //// options: nested, expanded, compact, compressed
-        //outputStyle: 'nested',
-		//
-        //// how many digits after the decimal will be allowed
-        //precision: 2,
-		//
-        //// additional debugging information in the output file as CSS comments
-        //sourceComments: false,
+        // main source entry point
+        sourceFile: 'main.js',
 
-        // the writing location for the source map file
-        // options: file name, true - inline source map, false - disable
-        sourceMap: 'develop.map'
+        // directory to store output files
+        targetPath: path.join(config.default.targetPath, 'js'),
 
-        //// whether to include the source files content in the source map
-        //// bigger map file but no need to serve source scss files
-        //sourceMapContents: false
-    },
-
-    // config for webpack:build:release task
-    release: {
         // intended output file name
         targetFile: 'release.js',
 
         //// whether to use space or tab character for indentation
         //indentType: 'space',
-		//
+        //
         //// the number of spaces or tabs to be used for indentation
         //indentWidth: 4,
-		//
+        //
         //// whether to use cr, crlf, lf or lfcr sequence for line break
         //linefeed: 'lf',
-		//
+        //
         //// output format of the final CSS style
         //// options: nested, expanded, compact, compressed
         //outputStyle: 'compressed',
-		//
+        //
         //// how many digits after the decimal will be allowed
         //precision: 2,
-		//
+        //
         //// additional debugging information in the output file as CSS comments
         //sourceComments: false,
 
@@ -79,5 +54,11 @@ module.exports = {
         //// whether to include the source files content in the source map
         //// bigger map file but no need to serve source scss files
         //sourceMapContents: false
+    },
+
+    develop: {
+        targetFile: 'develop.js',
+
+        sourceMap: 'develop.map'
     }
-};
+});
